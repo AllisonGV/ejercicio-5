d44474dc4e33acc773c2fb4841c9c5678c5fc268
AllisonGV <86426801+AllisonGV@users.noreply.github.com>
2021-07-02 17:10:48 -0600

    TRABAJO
    MATRIZ RANDOM

---
+++ EJERCICIO5/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace EJERCICIO5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoFilas = new System.Windows.Forms.TextBox();
+            this.textBoXColum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 51);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "No Filas:";
+            // 
+            // textBoFilas
+            // 
+            this.textBoFilas.Location = new System.Drawing.Point(332, 75);
+            this.textBoFilas.Name = "textBoFilas";
+            this.textBoFilas.Size = new System.Drawing.Size(69, 22);
+            this.textBoFilas.TabIndex = 10;
+            // 
+            // textBoXColum
+            // 
+            this.textBoXColum.Location = new System.Drawing.Point(332, 47);
+            this.textBoXColum.Name = "textBoXColum";
+            this.textBoXColum.Size = new System.Drawing.Size(69, 22);
+            this.textBoXColum.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "No Columnas:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tamaño de Matriz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoFilas);
+            this.Controls.Add(this.textBoXColum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoFilas;
+        private System.Windows.Forms.TextBox textBoXColum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EJERCICIO5/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int columnas;
+            int filas;
+            filas = Convert.ToInt32(textBoFilas.Text);
+            columnas = Convert.ToInt32(textBoXColum.Text);
+            int[,] matrizEjemplo = new int[filas, columnas];
+            Random aleatorio = new Random();
+
+            for (int IndiceFila = 0; IndiceFila < matrizEjemplo.GetLength(0); IndiceFila++)
+            {
+                for (int IndiceColumna = 0; IndiceColumna < matrizEjemplo.GetLength(1); IndiceColumna++)
+                {
+                    matrizEjemplo[IndiceFila, IndiceColumna] = aleatorio.Next(100);
+                }
+            }
+            for (int IndiceFila = 0; IndiceFila < matrizEjemplo.GetLength(0); IndiceFila++)
+            {
+                for (int IndiceColumna = 0; IndiceColumna < matrizEjemplo.GetLength(1); IndiceColumna++)
+                {
+                    MessageBox.Show("La posicion es : " + IndiceFila + "," + IndiceColumna + " = " + matrizEjemplo[IndiceFila, IndiceColumna].ToString());
+                }
+            }
+        }
+    }
+}
